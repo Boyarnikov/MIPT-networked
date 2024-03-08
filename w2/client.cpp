ac6e7f73419dfc566feae0daafdd5c32af92af0e
Boyar <boiarnikov.ia@phystech.edu>
2024-03-09 00:01:12 +0300

    created client-lobby connections

--- w2/client.cpp
+++ w2/client.cpp
@@ -1,36 +1,38 @@
 #include "raylib.h"
 #include <enet/enet.h>
 #include <iostream>
+#include <sstream>
+#include <map>
 
-void send_fragmented_packet(ENetPeer *peer)
+bool debug_string = false;
+bool info_string = true;
+uint32_t ping_periud = 1000;
+
+enum class MsgHeader {RequestGame, GotRedirectToGame, PlayerJoinedGame, AllPlayers, PlayersPings, SendPositionUpdate, Ping, Pong, ReceveName, UpdatePositions};
+
+void SendStringReliable(ENetPeer* peer, std::string &str)
 {
-  const char *baseMsg = "Stay awhile and listen. ";
-  const size_t msgLen = strlen(baseMsg);
-
-  const size_t sendSize = 2500;
-  char *hugeMessage = new char[sendSize];
-  for (size_t i = 0; i < sendSize; ++i)
-    hugeMessage[i] = baseMsg[i % msgLen];
-  hugeMessage[sendSize-1] = '\0';
-
-  ENetPacket *packet = enet_packet_create(hugeMessage, sendSize, ENET_PACKET_FLAG_RELIABLE);
-  enet_peer_send(peer, 0, packet);
-
-  delete[] hugeMessage;
+  if (debug_string)
+    std::cout << "sending string " << str << " with length " << strlen(str.c_str()) + 1 << std::endl;
+	ENetPacket* packet = enet_packet_create(str.c_str(), strlen(str.c_str()) + 1, ENET_PACKET_FLAG_RELIABLE);
+	enet_peer_send(peer, 0, packet);
 }
 
-void send_micro_packet(ENetPeer *peer)
+void SendStringFragment(ENetPeer* peer, std::string &str)
 {
-  const char *msg = "dv/dt";
-  ENetPacket *packet = enet_packet_create(msg, strlen(msg) + 1, ENET_PACKET_FLAG_UNSEQUENCED);
-  enet_peer_send(peer, 1, packet);
+  if (debug_string)
+    std::cout << "sending fragment " << str << " with length " << strlen(str.c_str()) + 1 << std::endl;
+	ENetPacket* packet = enet_packet_create(str.c_str(), strlen(str.c_str()) + 1, ENET_PACKET_FLAG_UNRELIABLE_FRAGMENT);
+	enet_peer_send(peer, 1, packet);
 }
 
 int main(int argc, const char **argv)
 {
+  // ================================================ Window init ================================================ //
+
   int width = 800;
   int height = 600;
-  InitWindow(width, height, "w6 AI MIPT");
+  InitWindow(width, height, "MyClient");
 
   const int scrWidth = GetMonitorWidth(0);
   const int scrHeight = GetMonitorHeight(0);
@@ -40,26 +42,36 @@ int main(int argc, const char **argv)
     height = std::min(scrHeight - 150, height);
     SetWindowSize(width, height);
   }
+  SetTargetFPS(60);
 
-  SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+  // ================================================= ENet init ================================================= //
 
   if (enet_initialize() != 0)
   {
     printf("Cannot init ENet");
     return 1;
   }
+  atexit(enet_deinitialize);
 
-  ENetHost *client = enet_host_create(nullptr, 1, 2, 0, 0);
+
+  // ============================================= Creating client =============================================== //
+
+  ENetHost *client = enet_host_create(nullptr, 2, 2, 0, 0); // client/host, connections, channels, in and out bandwidth
   if (!client)
   {
     printf("Cannot create ENet client\n");
     return 1;
   }
+  atexit(enet_deinitialize);
+
+  // =========================================== Connecting to lobby ============================================= //
 
   ENetAddress address;
+  
   enet_address_set_host(&address, "localhost");
   address.port = 10887;
 
+  ENetPeer *gamePeer;
   ENetPeer *lobbyPeer = enet_host_connect(client, &address, 2, 0);
   if (!lobbyPeer)
   {
@@ -67,60 +79,255 @@ int main(int argc, const char **argv)
     return 1;
   }
 
+  // ======================================= Lobby Connection handling =========================================== //
   uint32_t timeStart = enet_time_get();
-  uint32_t lastFragmentedSendTime = timeStart;
-  uint32_t lastMicroSendTime = timeStart;
+  uint32_t lastStartGameSended = timeStart;
   bool connected = false;
+  bool connected_to_game = false;
   float posx = 0.f;
   float posy = 0.f;
-  while (!WindowShouldClose())
+
+
+  std::map<std::string, int> m{};
+  // =============================================== Lobby loop ================================================== //
+  while (!WindowShouldClose() && !connected_to_game)
   {
     const float dt = GetFrameTime();
     ENetEvent event;
-    while (enet_host_service(client, &event, 10) > 0)
+    while (enet_host_service(client, &event, 10) > 0 && !connected_to_game)
     {
+      std::string data;
+      std::string delimiter = "||";
+      std::string token;
+      int int_data;
+      MsgHeader header;
       switch (event.type)
       {
       case ENET_EVENT_TYPE_CONNECT:
-        printf("Connection with %x:%u established\n", event.peer->address.host, event.peer->address.port);
+        if (debug_string)
+          printf("Connection with %x:%u established\n", event.peer->address.host, event.peer->address.port);
         connected = true;
         break;
       case ENET_EVENT_TYPE_RECEIVE:
-        printf("Packet received '%s'\n", event.packet->data);
+        data = (char*)event.packet->data;
+        if (debug_string)
+          printf("Packet received '%s'\n", event.packet->data);
+
+        token = data.substr(0, data.find(delimiter));
+        header = (MsgHeader)std::stoi(token);
+
+        switch (header)
+        {
+        case MsgHeader::GotRedirectToGame:
+          data.erase(0, data.find(delimiter) + delimiter.length());
+          if (info_string)
+            std::cout << "Redirected to game at " << data << std::endl;
+
+          delimiter = ":";
+          token = data.substr(0, data.find(delimiter));
+          data.erase(0, data.find(delimiter) + delimiter.length());
+          int_data = std::stoi(data);
+
+          enet_address_set_host(&address, token.c_str());
+          address.port = int_data;
+
+          gamePeer = enet_host_connect(client, &address, 2, 0);
+          if (!gamePeer)
+          {
+            printf("Cannot connect to game");
+            return 1;
+          }
+          connected_to_game = true;
+          break;
+        default:
+          if (debug_string)
+            printf("Command is not processed due to not having it in MsgHeader\n");
+          break;
+        }
         enet_packet_destroy(event.packet);
+
         break;
       default:
         break;
       };
     }
+
+    // Sending data
+
+    uint32_t curTime = 0;
     if (connected)
     {
+      // timestamp in ms
       uint32_t curTime = enet_time_get();
-      if (curTime - lastFragmentedSendTime > 1000)
-      {
-        lastFragmentedSendTime = curTime;
-        send_fragmented_packet(lobbyPeer);
-      }
-      if (curTime - lastMicroSendTime > 100)
-      {
-        lastMicroSendTime = curTime;
-        send_micro_packet(lobbyPeer);
+    
+      // sending 
+      bool enter = IsKeyDown(KEY_ENTER);
+      if (enter && curTime - lastStartGameSended > 1000 && !connected_to_game) {
+          lastStartGameSended = curTime;
+          std::string request = std::to_string((int)MsgHeader::RequestGame) + "||";
+          
+          SendStringReliable(lobbyPeer, request);
       }
     }
-    bool left = IsKeyDown(KEY_LEFT);
-    bool right = IsKeyDown(KEY_RIGHT);
-    bool up = IsKeyDown(KEY_UP);
-    bool down = IsKeyDown(KEY_DOWN);
-    constexpr float spd = 10.f;
-    posx += ((left ? -1.f : 0.f) + (right ? 1.f : 0.f)) * dt * spd;
-    posy += ((up ? -1.f : 0.f) + (down ? 1.f : 0.f)) * dt * spd;
 
-    BeginDrawing();
-      ClearBackground(BLACK);
-      DrawText(TextFormat("Current status: %s", "unknown"), 20, 20, 20, WHITE);
-      DrawText(TextFormat("My position: (%d, %d)", (int)posx, (int)posy), 20, 40, 20, WHITE);
-      DrawText("List of players:", 20, 60, 20, WHITE);
-    EndDrawing();
+
+
+    if (!connected_to_game) {
+      BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText(TextFormat("Press enter to request a game start"), 20, 20, 20, WHITE);
+      EndDrawing();
+    }
   }
+
+  std::string name = "";
+  std::map<std::string, int> pings{};
+  std::map<std::string, std::pair<int, int>> positions{};
+
+  // =============================================== Game loop ================================================== //
+    uint32_t lastPositionSended = enet_time_get();
+    while (!WindowShouldClose())
+    {
+      const float dt = GetFrameTime();
+      ENetEvent event;
+      while (enet_host_service(client, &event, 10) > 0)
+      {
+        std::string data;
+        std::string delimiter = "||";
+        std::string token;
+        int position;
+        MsgHeader header;
+        switch (event.type)
+        {
+        case ENET_EVENT_TYPE_CONNECT:
+          if (debug_string)
+            printf("Connection with %x:%u established\n", event.peer->address.host, event.peer->address.port);
+          connected = true;
+          break;
+        case ENET_EVENT_TYPE_RECEIVE:
+          data = (char*)event.packet->data;
+          if (debug_string)
+            printf("Packet received '%s'\n", event.packet->data);
+
+          token = data.substr(0, data.find(delimiter));
+          header = (MsgHeader)std::stoi(token);
+
+          switch (header)
+          {
+          case MsgHeader::Ping:
+            data.erase(0, data.find(delimiter) + delimiter.length());
+            token = std::to_string((int)MsgHeader::Pong) + "||" + std::to_string(std::stoi(data));
+            SendStringFragment(event.peer, token);
+            break;
+          case MsgHeader::PlayersPings:
+            data.erase(0, data.find(delimiter) + delimiter.length());
+            while (data.length() > 0) {
+              token = data.substr(0, data.find(":"));
+              data.erase(0, data.find(":") + 1);
+              pings[token] = std::stoi(data.substr(0, data.find(",")));
+              data.erase(0, data.find(",") + 1);
+            }
+            break;
+          case MsgHeader::UpdatePositions:
+            if (info_string)
+              std::cout << "Recived positions " << std::endl;
+            data.erase(0, data.find(delimiter) + delimiter.length());
+            if (info_string)
+              std::cout << data << std::endl;
+            while (data.length() > 0) {
+              token = data.substr(0, data.find(":"));
+              data.erase(0, data.find(":") + 1);
+              position = std::stoi(data.substr(0, data.find(":")));
+              data.erase(0, data.find(":") + 1);
+              positions[token] = std::pair{position, std::stoi(data.substr(0, data.find(",")))};         
+              data.erase(0, data.find(",") + 1);    
+            }
+            break;
+          case MsgHeader::ReceveName:
+            if (info_string)
+              std::cout << "Recived name " << std::endl;
+            data.erase(0, data.find(delimiter) + delimiter.length());
+            name = data.substr(0, data.find(":"));
+            break;
+          case MsgHeader::AllPlayers:
+            if (info_string)
+              std::cout << "Recived all players: " << std::endl;
+            data.erase(0, data.find(delimiter) + delimiter.length());
+            if (info_string)
+              std::cout << data << std::endl;
+            break;
+          case MsgHeader::PlayerJoinedGame:
+            if (info_string)
+              std::cout << "New player joined: " << std::endl;
+            data.erase(0, data.find(delimiter) + delimiter.length());
+            if (info_string)
+              std::cout << data << std::endl;
+            break;
+          default:
+            if (debug_string)
+              printf("Command is not processed due to not having it in MsgHeader\n");
+            break;
+          }
+          enet_packet_destroy(event.packet);
+
+          break;
+        default:
+          break;
+        };
+      }
+
+      uint32_t curTime = 0;
+      bool left = IsKeyDown(KEY_LEFT);
+      bool right = IsKeyDown(KEY_RIGHT);
+      bool up = IsKeyDown(KEY_UP);
+      bool down = IsKeyDown(KEY_DOWN);
+      constexpr float spd = 10.f;
+      posx += ((left ? -1.f : 0.f) + (right ? 1.f : 0.f)) * dt * spd;
+      posy += ((up ? -1.f : 0.f) + (down ? 1.f : 0.f)) * dt * spd;
+
+      if (connected)
+      {
+        // timestamp in ms
+        uint32_t curTime = enet_time_get();
+      
+        // Slowed down to make logs readable
+        if (curTime - lastPositionSended > ping_periud) {
+            lastPositionSended = curTime;
+            std::string request = std::to_string((int)MsgHeader::SendPositionUpdate) + "||" + std::to_string(posx) + ":" + std::to_string(posy);
+            SendStringReliable(gamePeer, request);
+        }
+      }
+        
+      BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText(TextFormat("Current status: %s", "In Game"), 20, 20, 20, WHITE);
+        DrawText(TextFormat("My position: (%d, %d)", (int)posx, (int)posy), 20, 40, 20, WHITE);
+        DrawText(("My name: " + name).c_str(), 20, 60, 20, WHITE);
+        int p = 80;
+        for (auto i: pings) {
+          if (name != i.first) {
+            DrawText(TextFormat((i.first + ": ping %dms").c_str(), i.second), 20, p, 20, WHITE);
+            p += 20;
+          }
+        }
+
+        for (auto i: positions) {
+          if (name != i.first) {
+            if (pings[i.first] < 1000) {
+              DrawCircle(width/2 + i.second.first, height/2 + i.second.second, 5, GREEN);
+            }
+            else {
+              DrawCircle(width/2 + i.second.first, height/2 + i.second.second, 5, DARKGREEN);
+            }
+              
+            p += 20;
+          }
+        }
+
+        DrawCircle(width/2 + (int)(posx), height/2 + (int)(posy), 5, BLUE);
+      EndDrawing();
+    }
+
+  atexit(enet_deinitialize);
   return 0;
 }
--- w2/lobby.cpp
+++ w2/lobby.cpp
@@ -1,13 +1,45 @@
 #include <enet/enet.h>
 #include <iostream>
+#include <string>
 
+bool debug_string = false;
+bool info_string = true;
+uint32_t ping_periud = 1000;
+
+enum class MsgHeader {RequestGame, GotRedirectToGame, PlayerJoinedGame, AllPlayers, PlayersPings, SendPositionUpdate, Ping, Pong, ReceveName, UpdatePositions};
+
+void BroadcastString(ENetHost* server, std::string &str)
+{
+  if (debug_string)
+    std::cout << "broadcasting string " << str << " with length " << strlen(str.c_str()) + 1 << std::endl;
+  char *data = str.data();
+	ENetPacket* packet = enet_packet_create(data, strlen(data) + 1, ENET_PACKET_FLAG_RELIABLE);
+	enet_host_broadcast(server, 0, packet);
+}
+
+void SendString(ENetPeer* peer, std::string &str)
+{
+  if (debug_string)
+    std::cout << "sending string " << str << " with length " << strlen(str.c_str()) + 1 << std::endl;
+	char *data = str.data();
+	ENetPacket* packet = enet_packet_create(data, strlen(data) + 1, ENET_PACKET_FLAG_RELIABLE);
+	enet_peer_send(peer, 0, packet);
+}
+
+// Waiting for START packege, then redirect to game_lobby
 int main(int argc, const char **argv)
 {
+  // ================================================= ENet init ================================================= //
+
   if (enet_initialize() != 0)
   {
     printf("Cannot init ENet");
     return 1;
   }
+  atexit(enet_deinitialize);
+
+  // ============================================= Creating client =============================================== //
+
   ENetAddress address;
 
   address.host = ENET_HOST_ANY;
@@ -21,18 +53,60 @@ int main(int argc, const char **argv)
     return 1;
   }
 
+  // ================================================ Game Loop ================================================= //
+
+  bool game_started = false;
+
   while (true)
   {
     ENetEvent event;
     while (enet_host_service(server, &event, 10) > 0)
     {
+      std::string data;
+      std::string delimiter = "||";
+      std::string token;
+      MsgHeader header;
       switch (event.type)
       {
       case ENET_EVENT_TYPE_CONNECT:
-        printf("Connection with %x:%u established\n", event.peer->address.host, event.peer->address.port);
+        if (debug_string)
+          printf("Connection with %x:%u established\n", event.peer->address.host, event.peer->address.port);
+
+        if (game_started) {
+          if (info_string)
+            printf("Sending game server data to new client\n");
+          data = std::to_string((int)MsgHeader::GotRedirectToGame) + "||localhost:10888";
+          SendString(event.peer, data);
+        }
         break;
       case ENET_EVENT_TYPE_RECEIVE:
-        printf("Packet received '%s'\n", event.packet->data);
+        data = (char*)event.packet->data;
+        if (debug_string)
+          printf("Packet received '%s'\n", event.packet->data);
+
+        token = data.substr(0, data.find(delimiter));
+        header = (MsgHeader)std::stoi(token);
+
+        switch (header)
+        {
+        case MsgHeader::RequestGame:
+          if (!game_started) {
+            game_started = true;
+            if (info_string) {
+              printf("Game start request: starting the game");
+              printf("Sending game server data to all current piers");
+            }
+            
+
+            data = std::to_string((int)MsgHeader::GotRedirectToGame) + "||localhost:10888";
+            BroadcastString(server, data);
+          }
+          break;
+        default:
+          if (debug_string)
+            printf("Command is not processed due to not having it in MsgHeader\n");
+          break;
+        }
         enet_packet_destroy(event.packet);
         break;
       default:
@@ -42,8 +116,8 @@ int main(int argc, const char **argv)
   }
 
   enet_host_destroy(server);
-
   atexit(enet_deinitialize);
+
   return 0;
 }
 
